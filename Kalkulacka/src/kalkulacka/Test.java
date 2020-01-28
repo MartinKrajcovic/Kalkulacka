150160ca45a1448b809daf7e29483af87b611afd
mkrajcovic <mkrajcovic@krajcovicpc2.hq.gratex.com>
2020-01-28 16:47:38 +0100

    navrh formatovacej funkcie na presnost desatinnych miest

---
+++ Kalkulacka/src/kalkulacka/Test.java
@@ -0,0 +1,20 @@
+package kalkulacka;
+
+import java.util.Formatter;
+
+public class Test {
+	
+	/**
+	 * Tato metoda sluzi na zaokruhlenie celeho datoveho typu double na
+	 * pozadovanu presnost desatinnych miest.
+	 */
+	public static double formatDouble(double num, int decimalPoint) {
+		Formatter fmt = new Formatter();
+		String precision = "%." + decimalPoint + "f";
+		String str = fmt.format(precision, num).toString();
+		fmt.close();
+		if (str.contains(","))
+			str = str.replace(',', '.');
+		return Double.parseDouble(str);
+	}
+}
